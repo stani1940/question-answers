f12f1dead78b3410897e7b8d70c1d060039fc3ab
stani1940 <stanislav1940@abv.bg>
2021-10-08 19:52:23 +0300

    Fix show error`s message with modern blade syntax

--- resources/views/questions/form.blade.php
+++ resources/views/questions/form.blade.php
@@ -2,20 +2,20 @@
 <div class="form-group">
     <label for="question-title">{{ __('Question Title') }}</label>
     <input type="text" name="title" value="{{old('title',$question->title)}}" id="question-title" class="form-control {{$errors->has('title') ? 'is-invalid':''}}">
-    @if($errors->has('title'))
+    @error('title')
         <div class="invalid-feedback">
-            <strong>{{$errors->first('title')}}</strong>
+            <strong>{{$message}}</strong>
         </div>
-    @endif
+    @enderror
 </div>
 <div class="form-group">
     <label for="question-body">{{ __('Explain your question') }}</label>
     <textarea name="body" id="question-body" rows="10" class="form-control {{$errors->has('body') ? 'is-invalid':''}}">{{old('body',$question->body)}}</textarea>
-    @if($errors->has('body'))
+    @error('body')
         <div class="invalid-feedback">
-            <strong>{{$errors->first('body')}}</strong>
+            <strong>{{$message}}</strong>
         </div>
-    @endif
+    @enderror
 </div>
 <div class="form-group">
     <button type="submit" class=" btn btn-outline-primary btn-lg">{{$buttonText}}</button>
